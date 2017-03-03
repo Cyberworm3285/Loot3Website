6633664b091f222edb18cb17f0278931e71598fa
FlexBlade <dangofelix@gmail.com>
2017-03-03 22:01:56 +0100

    hrhrh
    ascaca

---
+++ Loot3Website/Loot3Website/Views/Home/includes/social.php
@@ -0,0 +1,17 @@
+<div class="socialsidebar <?= $side3 ?>">
+      <a href="https://twitter.com/FlexBlade"  target="_blank" class="socialbtn">
+        <div class="svg-icon twitter" >
+          <?php  echo file_get_contents('html/svg/twitter.svg'); ?>
+        </div>
+      </a>
+      <a href="https://www.youtube.com/channel/UCq8w6mVqDvi-AEz44Rego8g/" target="_blank" class="socialbtn">
+        <div class="svg-icon youtube" >
+          <?php  echo file_get_contents('html/svg/youtube.svg'); ?>
+        </div>
+      </a>
+      <a href="https://www.twitch.tv/theflexblade" target="_blank"  class="socialbtn" >
+        <div class="svg-icon twitch" >
+          <?php  echo file_get_contents('html/svg/twitch.svg'); ?>
+        </div>
+      </a>
+    </div>
